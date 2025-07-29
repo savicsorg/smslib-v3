5f04449ffa1d22799616d5bc83c2a56e2edd22f5
Philippe Magloire Ngimbis N <ngimbis@savics.org>
2025-07-29 13:03:22 +0100

    DAT-3248

--- src/java/org/smslib/helper/Logger.java
+++ src/java/org/smslib/helper/Logger.java
@@ -20,7 +20,20 @@
 
 package org.smslib.helper;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+
 public class Logger {
+    
+    public final String FILE_SEP = System.getProperty("file.separator");
+    public final String USER_DIR = System.getProperty("user.dir");
+    public final String COMMON_APP_DATA_FOLDER = (System.getProperty("os.name").contains("XP")) ? "Documents and Settings" + FILE_SEP + "All Users" + FILE_SEP + "Application Data" : "ProgramData";
+    public final String USER_HOME = System.getenv("SystemDrive") + FILE_SEP + COMMON_APP_DATA_FOLDER;
+    
     private static Logger logger = new Logger();
 
     public static Logger getInstance() {
@@ -28,34 +41,111 @@ public class Logger {
     }
 
     public void logInfo(String message, Exception e, String gatewayId) {
-        System.out.println(formatMessage(message, gatewayId));
-        if (e != null){
-            e.printStackTrace();
+        Properties properties = getTextoPropertiesValues();
+        if (properties == null) {
+            //throw new IllegalArgumentException("Properties cannot be null");
+            System.out.println("Properties cannot be null");
+        }
+        String logLevelStr = properties.getProperty("log.level").toUpperCase();
+        if (logLevelStr.equals("INFO")) { 
+            System.out.println(formatMessage(message, gatewayId));
+            if (e != null){
+                e.printStackTrace();
+            }
         }
     }
 
     public void logWarn(String message, Exception e, String gatewayId) {
-        System.out.println(formatMessage(message, gatewayId));
-        if (e!= null){
-            e.printStackTrace();
+        Properties properties = getTextoPropertiesValues();
+        if (properties == null) {
+            //throw new IllegalArgumentException("Properties cannot be null");
+            System.out.println("Properties cannot be null");
+        }
+        String logLevelStr = properties.getProperty("log.level").toUpperCase();
+        if (logLevelStr.equals("WARNING")) { 
+            System.out.println(formatMessage(message, gatewayId));
+            if (e!= null){
+                e.printStackTrace();
+            }
         }
     }
 
     public void logDebug(String message, Exception e, String gatewayId) {
-//        System.err.println(formatMessage(message, gatewayId));
-//        if (e!= null){
-//            e.printStackTrace();
-//        }
+        Properties properties = getTextoPropertiesValues();
+        if (properties == null) {
+            //throw new IllegalArgumentException("Properties cannot be null");
+            System.out.println("Properties cannot be null");
+        }
+        String logLevelStr = properties.getProperty("log.level").toUpperCase();
+        if (logLevelStr.equals("DEBUG")) { 
+            System.err.println(formatMessage(message, gatewayId));
+            if (e!= null){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void logError(String message, Exception e, String gatewayId) {
-        System.err.println(formatMessage(message, gatewayId));
-        if (e!= null){
-            e.printStackTrace();
+        Properties properties = getTextoPropertiesValues();
+        if (properties == null) {
+            //throw new IllegalArgumentException("Properties cannot be null");
+            System.out.println("Properties cannot be null");
+        }
+        String logLevelStr = properties.getProperty("log.level").toUpperCase();
+        if (logLevelStr.equals("ERROR")) { 
+            System.err.println(formatMessage(message, gatewayId));
+            if (e!= null){
+                e.printStackTrace();
+            }
         }
     }
 
     private String formatMessage(String message, String gatewayId) {
         return (gatewayId == null) ? message : "GTW: " + gatewayId + ": " + message;
     }
+    
+    public Properties getTextoPropertiesValues() {
+        String file = "Texto.properties";
+        String directory = USER_HOME + FILE_SEP + "Savics" + FILE_SEP + "DataToCare" + FILE_SEP + "Biopics" + FILE_SEP + "Biopics";
+        String location = directory + FILE_SEP + file;
+        try {
+            File f = new File(location);
+            if (!f.exists() || f.isDirectory()) {
+                location = file;
+
+                File f2 = new File(location);
+                if (!f2.exists() || f2.isDirectory()) {
+                    location = USER_DIR + FILE_SEP + "plugins" + FILE_SEP + "texto" + FILE_SEP + file;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Properties properties = new Properties();
+
+        File f = new File(location);
+        if (f.exists() && !f.isDirectory()) {
+            InputStream input = null;
+            try {
+
+                input = new FileInputStream(location);
+                // load a properties file
+                properties.load(input);
+            } catch (IOException ex) {
+                java.util.Locale.setDefault(java.util.Locale.ENGLISH);
+                ex.printStackTrace();
+            } finally {
+                if (input != null) {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return properties;
+    }
+    
 }
